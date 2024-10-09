4b26ab83c09a43e959e77fb84ee0bce71db67560
uday-mac <udaysri1921@gmail.com>
2024-10-09 11:37:11 +0530

    updated kops.sh

--- kops/kops.sh
+++ kops/kops.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kops create cluster --name=kops.udaymac.xyz --zones=us-east-1a,us-east-1b \
 --state=s3://kops-state-v2 --node-count=1 \
---node-size=t3.micro --master-size=t3.small \
+--node-size=t3.small --master-size=t3.medium \
 --node-volume-size=8 --master-volume-size=8 --dns-zone=kops.udaymac.xyz
