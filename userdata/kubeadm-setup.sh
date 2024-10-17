85e66a7aff52536ff41245476eb7fe2f2b7f7042
uday-mac <udaysri1921@gmail.com>
2024-10-17 16:56:26 +0530

    added setup script for kubeadm

---
+++ userdata/kubeadm-setup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#installing the docker engine
+
+yum --help &>> /dev/null
+if [ $? -eq 0 ]
+then 
+  sudo yum install -y yum-utils
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  sudo systemctl start docker
+  sudo systemctl enable docker
+else
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc  
+
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  sudo apt-get update
+
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+  sudo systemctl enable docker
+
+fi
+
+sleep 30
+
+#installing kubeadm,kubelet and kubectl
+yum --help &>> /dev/null
+if [ $? -eq 0 ]
+then
+  sudo setenforce 0
+  sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+  cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+    [kubernetes]
+    name=Kubernetes
+    baseurl=https://pkgs.k8s.io/core:/stable:/v1.31/rpm/
+    enabled=1
+    gpgcheck=1
+    gpgkey=https://pkgs.k8s.io/core:/stable:/v1.31/rpm/repodata/repomd.xml.key
+    exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF  
+  
+  sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+  sudo systemctl enable --now kubelet
+
+else
+  sudo apt-get update
+
+  sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+
+  curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+  echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+  sudo apt-get update
+  sudo apt-get install -y kubelet=1.30.0-1.1 kubeadm=1.30.0-1.1 kubectl=1.30.0-1.1
+  sudo apt-mark hold kubelet kubeadm kubectl 
+
+  sudo systemctl enable --now kubelet
+
+fi
+
+
+  
